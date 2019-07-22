77e06bc9130b494c14ee6522c1742f4275b8820c
Uli Köhler <ulikoehler@online.de>
2019-07-22 05:06:34 +0200

    Started to add copy-since sourcecode

---
+++ copy-since-prepare.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from datetime import datetime
+import os
+import argparse
+
+def write_reference_timestamp_to_file(filename):
+    with open(filename, "w") as timestampfile:
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        timestampfile.write(now)
+
+def set_file_access_time(filename, atime):
+    """
+    Set the access time of a given filename to the given atime.
+    atime must be a datetime object.
+    """
+    stat = os.stat(filename)
+    mtime = stat.st_mtime
+    os.utime(filename, times=(atime.timestamp(), mtime))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("from", help="The directory to copy from")
+    args = parser.parse_args()
+
+    write_reference_timestamp_to_file("copy-since-timestamp.txt")
+
+    # Set "really old" access time on all those files
+    # This forces "relatime"-style mounted filesystems to update on access!
+    atime = datetime(2000, 1, 1, 0, 0, 0)
+    for subdir, dirs, files in os.walk(vars(args)["from"]):
+        for file in files:
+            set_file_access_time(os.path.join(subdir, file), atime)
+            
---
+++ copy-since.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import os
+import argparse
+from datetime import datetime
+
+def read_reference_timestamp_from_file(filename):
+    with open(filename) as timestampfile:
+        return datetime.strptime("%Y-%m-%d %H:%M:%S", timestampfile.read().strip())
+
+
+def last_file_access_time(filename):
+    """
+    Get a datetime() representing the last access time of the given file.
+    The returned datetime object is in local time
+    """
+    return datetime.fromtimestamp(os.stat(filename).st_atime)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("from", help="The directory to copy from")
+    parser.add_argument("to", help="The directory to copy to")
+    args = parser.parse_args()
+
+    # 
+    reftime = write_reference_timestamp_to_file("copy-since-timestamp.txt")
+    print(reftime)
+
+    for subdir, dirs, files in os.walk(vars(args)["from"]):
+        for file in files:
+            filepath = os.path.abspath(os.path.join(subdir, file))
+            atime = last_file_access_time(filepath)
+            if atime < reftime:
+                continue
+            print(filepath)
